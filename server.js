637e95f916803b5296ea7212f0cfee06e0ac093b
AdrianRydin <adrian.rydin@gmail.com>
2025-05-29 16:33:23 +0200

    fixade grunder

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import errorHandler from "./middlewares/errorHandler.js";
+import logger from "./middlewares/logger.js";
+import authRouter from "./routes/auth.js";
+
+// Config
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+mongoose.connect(process.env.CONNECTION_STRING);
+const database = mongoose.connection;
+
+// Middlewares
+
+app.use(express.json());
+app.use(logger);
+
+// Routes
+
+app.use("/api/auth", authRouter);
+
+// DB
+
+database.on("error", (error) => console.log(error));
+database.once("connected", () => {
+  console.log("DB Connected");
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
+
+// Error handling
+
+app.use(errorHandler);
